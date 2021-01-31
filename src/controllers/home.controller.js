ef52b1026b776e6b31260915ea0a9c620b67dbe7
Nestor Cortina <junior110120@gmail.com>
2021-01-31 01:59:10 -0500

    chore: page lista

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,8 +1,9 @@
 import home from '../view/home.html'
 
 export default () => {
-  const divElement = document.createElement('div');
-  divElement.innerHTML = home;
+  const divElement = document.createElement('div')
+  divElement.classList.add('container', 'pr-0', 'mr-0')
+  divElement.innerHTML = home
 
-  return divElement;
+  return divElement
 }
