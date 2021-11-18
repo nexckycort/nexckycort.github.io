58656f4a8f5f60f753d88e8172694ec2bbbdee18
Nestor Cortina <nexckycort@gmail.com>
2021-11-18 16:48:35 -0500

    feat: add footer (#2)
    * feat: add footer
    
    * style: format code

--- src/components/layout/footer/index.ts
+++ src/components/layout/footer/index.ts
@@ -1,4 +1,36 @@
+import socialMedia from '../../../data/socialMedia.json'
+
+import { footer } from '../../../lang/es.json'
+
+import style from './style.module.scss'
+
 const Footer = () => {
-  return /* html */ ''
+  const footerElement = document.createElement('footer')
+  footerElement.classList.add(style.footer)
+  footerElement.innerHTML = /* html */ `
+  <div class="${style.body}">
+    <div>
+      <ul class="${style.socialMedia}">
+        ${socialMedia
+          .map(({ id, name, url }) => {
+            return `
+            <li id="${id}">
+              <a class="${style.mediaLink} lined-link" href="${url}" target="_blank" rel="noopener noreferrer">
+                ${name}
+              </a>
+            </li>`
+          })
+          .join('')}
+      </ul>
+    </div>
+    <div>
+      <p class="${style.quote}" data-section="footer" data-value="quote">${footer.quote}</p>
+      <a class="${
+        style.btnContactMe
+      }" href="mailto:nexckycort@gmail.com" data-section="footer" data-value="contactMe">${footer.contactMe}</a>
+    </div>
+  </div>`
+
+  return footerElement
 }
 export default Footer
