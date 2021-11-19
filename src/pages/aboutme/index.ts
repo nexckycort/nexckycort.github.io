f41ca37f0dad2f4eb80c3931850779b7c720b5bc
nexckycort <nexckycort@gmail.com>
2021-11-18 21:43:20 -0500

    feat: add description

--- src/pages/aboutme/index.ts
+++ src/pages/aboutme/index.ts
@@ -3,13 +3,11 @@ import style from './style.module.scss'
 import { profile } from '../../lang/es.json'
 
 export const AboutMePage = async () => {
-  const me = await import('../../assets/img/me.jpg').then((module) => module.default)
-
   const divElement = document.createElement('div')
   divElement.classList.add(style.aboutMeWrapper)
   divElement.innerHTML = /* html */ `
     <div class="${style.meWrapper}">
-      <img src="${me}" class="${style.me}" alt="me" />
+      <img src="/me.jpg" class="${style.me}" alt="me" />
       <h2 class="${style.rol} data-section="profile" data-value="rol"">${profile.rol}</h2>
     </div>
     <div class="${style.aboutMe}">
