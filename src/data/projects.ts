7eb033fcec5f7a07cb7500fde766225edc139d6e
nexckycort <nexckycort@gmail.com>
2021-11-18 20:03:41 -0500

    feat: add masonry layout

---
+++ src/data/projects.ts
@@ -0,0 +1,86 @@
+interface Projects {
+  title: string
+  slug: string
+  image: string
+  category: string
+  description: string
+  tags: string[]
+  sourceCode: string
+  demoView: string | null
+}
+
+const projects: Projects[] = [
+  {
+    title: 'backend clean architecture nodejs',
+    slug: 'backend-clean-architecture-nodejs',
+    image: 'https://user-images.githubusercontent.com/50475272/137671622-13fa257b-eafe-40f5-a2c5-e2f1070015a8.jpg',
+    category: 'Software Architecture',
+    description:
+      'This architecture is an extensive investigation, from my experience dealing with a poorly structured node.js project, bad patterns, and countless hours of code refactoring and moving things from one place to another.',
+    tags: [
+      'nodejs',
+      'typescript',
+      'patterns-design',
+      'solid',
+      'docker',
+      'unit-test',
+      'e2e-test',
+      'webpack',
+      'pipeline'
+    ],
+    sourceCode: 'https://github.com/nexckycort/backend-clean-architecture-nodejs',
+    demoView: null
+  },
+  {
+    title: 'rijndael nodejs',
+    slug: 'rijndael-nodejs',
+    image: 'https://user-images.githubusercontent.com/50475272/137672239-c0f352b8-c3c0-4486-ac96-d57d0481d83e.png',
+    category: 'Software Architecture',
+    description: 'Implementation of the rijndael encryption algorithm.',
+    tags: ['nodejs', 'typescript', 'patterns-design', 'solid', 'unit-test', 'webpack', 'cryptography'],
+    sourceCode: 'https://github.com/nexckycort/rijndael-nodejs',
+    demoView: null
+  },
+  {
+    title: 'monolito react nodejs',
+    slug: 'monolito-react-nodejs',
+    image: 'https://user-images.githubusercontent.com/50475272/137674443-5f0fdc49-29ee-431f-9692-67c360dacc32.png',
+    category: 'Software Architecture',
+    description:
+      'Implementation of web application with client server architecture with the functionality of merge both projects when compiling in order to deploy a monolithic app.',
+    tags: [
+      'nodejs',
+      'react',
+      'typescript',
+      'patterns-design',
+      'solid',
+      'unit-test',
+      'webpack',
+      'monolith-architecture'
+    ],
+    sourceCode: 'https://github.com/nexckycort/monolito-react-nodejs',
+    demoView: 'https://monolito-react-nodejs.herokuapp.com'
+  },
+  {
+    title: 'frontend clean architecture reactjs',
+    slug: 'frontend-clean-architecture-reactjs',
+    image: 'https://user-images.githubusercontent.com/50475272/137676002-1e2f8214-c3a4-470b-84bf-2e866f16fd26.png',
+    category: 'Software Architecture',
+    description: 'This is a clean frontend architecture project .',
+    tags: ['react', 'typescript', 'patterns-design', 'solid'],
+    sourceCode: 'https://github.com/nexckycort/frontend-clean-architecture-reactjs',
+    demoView: null
+  },
+  {
+    title: 'keycloak demo react',
+    slug: 'keycloak-demo-react',
+    image: 'https://user-images.githubusercontent.com/50475272/137674242-ea6f2de6-3c9d-477a-86ca-039ab35a00c4.png',
+    category: 'Single Sign-On',
+    description: 'Demo for React and Keycloak SSO integration.',
+    tags: ['react', 'typescript', 'keycloak'],
+    sourceCode: 'https://github.com/nexckycort/keycloak-demo-react',
+    demoView: null
+  }
+]
+
+export default projects
--- src/pages/portfolio/index.ts
+++ src/pages/portfolio/index.ts
@@ -1,8 +1,24 @@
+import projects from '../../data/projects'
+
 import style from './style.module.scss'
 
 export const PortfolioPage = async () => {
   const divElement = document.createElement('div')
-  divElement.classList.add(style.construction)
-  divElement.innerHTML = /* html */ `<div class="${style.construction}">Comming soon</div>`
+  divElement.classList.add(style.container)
+  divElement.innerHTML = /* html */ `
+    <div class="${style.gallery}">
+      ${projects
+        .map(({ slug, image, title }) => {
+          return `
+            <div class="${style.wrapperImage}">
+              <a href="#/works/${slug}">
+                <span class="${style.workTitle}">${title}</span>
+              </a>
+              <img class="${style.workImage}" src="${image}" alt="${title}" />
+            </div>`
+        })
+        .join('')}
+    </div>`
+
   return divElement
 }
