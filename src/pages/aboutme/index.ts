cb2c588e1ac2016cffb0a67becf23941f89de6aa
Nestor Cortina <junior110120@gmail.com>
2022-01-05 15:07:15 -0500

    feat: add modal view cv

--- src/pages/aboutme/index.ts
+++ src/pages/aboutme/index.ts
@@ -15,8 +15,34 @@ export const AboutMePage = async () => {
     <div class="${style.aboutMe}">
       <h1 class="${style.title}" data-section="profile" data-value="title">${profile.title}</h1>
       <p class="${style.description}" data-section="profile" data-value="description">${profile.description}</p>
-      <a class="${style.resume}" href="https://bit.ly/3FZSVca" target="_blank" rel="noopener" data-section="profile" data-value="cv">${profile.cv}</a>
+      <a class="${style.resume}" id="btnModalViewCV" rel="noopener" data-section="profile" data-value="cv">${profile.cv}</a>
+    </div>
+    <div id="modalViewCV" class="${style.modal}">
+      <div class="${style.modalContent}">
+        <span class="${style.modalClose}">&times;</span>
+        <iframe class="${style.cv}" src="https://drive.google.com/file/d/1NU44MD0QEHZ9XSHlh1pQsLwqunrMn5t3/preview"></iframe>
+      </div>
     </div>`
+
+  const modalElement = divElement.querySelector('#modalViewCV') as HTMLElement
+  const btnModalViewCVElement = divElement.querySelector('#btnModalViewCV') as Element
+
+  btnModalViewCVElement.addEventListener('click', (e) => {
+    e.preventDefault()
+    modalElement.style.display = 'block'
+  })
+
+  const closeModalElement = divElement.getElementsByClassName(style.modalClose)[0]
+  closeModalElement.addEventListener('click', (e) => {
+    e.preventDefault()
+    modalElement.style.display = 'none'
+  })
+
+  window.onclick = function (event) {
+    if (event.target === modalElement) {
+      modalElement.style.display = 'none'
+    }
+  }
 
   return divElement
 }
